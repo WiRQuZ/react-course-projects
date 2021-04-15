be7961cced41a0dbea9e94643dab0f8f96273588
WiRQuZ <40850920+WiRQuZ@users.noreply.github.com>
2021-04-15 22:13:30 +0300

    Initial commit

---
+++ test0409/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Take it ease! on<a href="https://codepen.io/manishady/pen/xGYvEK"> side </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
